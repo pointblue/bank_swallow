d6e471cd282f42443de7dd1722bfe3c5e102bb14
Kristen Dybala <kdybala@gmail.com>
2024-05-23 15:46:20 -0700

    add effect size plot and stub for rip rap plot

--- R/6_figs.R
+++ R/6_figs.R
@@ -5,7 +5,8 @@ source('R/packages.R')
 source('R/functions_figs.R')
 
 modeldat = read_csv('data/modeldat.csv') |> filter(WY >= 1999)
-load('output/mod1_final_20240514.Rdata')
+load('output/model_results.RData')
+model_stats = read_csv('output/model_stats.csv')
 
 # covariates-----------
 
@@ -14,33 +15,40 @@ ggsave(filename = 'fig/covariates.png', width = 6.5, height = 5, units = 'in')
 
 # observed vs. predicted-----------
 # growth rates and burrow counts
-plot_observed_predicted(mod1_res, obsdat = modeldat, HPD = TRUE, pg0 = TRUE)
+plot_observed_predicted(mod_res_final, obsdat = modeldat, HPD = TRUE, pg0 = TRUE)
 ggsave(filename = 'fig/observed_predicted.png', width = 6.5, height = 5, units = 'in')
 
 # partial effects plots---------
 # original predictor values from inputdat1
-partial_effects = compile_partial_effects(mod1_res, param = 'R.pred',
-                                          covariates = inputdat1$predictors, 
-                                          predicted = inputdat1$predmatrix)
+partial_effects = compile_partial_effects(mod_res_final, param = 'R.pred',
+                                          covariates = inputdat_final$predictors, 
+                                          predicted = inputdat_final$predmatrix)
 
-plot_partial_effects(partial_effects, obsdat = modeldat)
-ggsave(filename = 'fig/partial_effects.png', width = 6.5, height = 7, units = 'in')
+plot_partial_effects(partial_effects, obsdat = modeldat, ylim = c(0.5, 1.7),
+                     year_label = c(2001, 2010, 2015, 2017, 2018, 2022, 2023), 
+                     pg0 = FALSE)
+ggsave(filename = 'fig/partial_effects.png', width = 5.5, height = 8.5, units = 'in')
 
-# plot effect sizes
-effect_stats %>% 
-  filter(grepl('beta|k', var)) %>% 
-  select(predictor, mean, lcl = `95%_HPDL`, ucl = `95%_HPDU`, pg0 = `p>0`) %>% 
-  mutate(label = if_else(round(pg0, 2) >= 0.9 | round(pg0, 2) <= 0.1, 
-                         '*', ''),
-         predictor = factor(predictor, 
-                            levels = rev(c('flow_concurrent', 'flow_prior', 'flow_older2',
-                                           'drought_prior', 
-                                           'density_int', 'density_trend')))) %>% 
-  ggplot(aes(predictor, mean)) +
-  geom_hline(aes(yintercept = 0), linetype = 'dashed') +
-  geom_errorbar(aes(ymin = lcl, ymax = ucl), width = 0.25) +
-  geom_point() + 
-  geom_text(aes(y = ucl + 0.01, label = label), size = 4) +
-  labs(x = NULL, y = 'effect size') + 
-  theme_classic() +
-  coord_flip()
+# effect size plot----------
+
+model_stats |> filter(grepl('beta', var)) |> 
+  mutate(varname = gsub('log.', '', varname),
+         across(c(mean, sd, `95%_HPDL`, `95%_HPDU`), 
+                ~if_else(varname == 'N', .*1000, .))) |> 
+  plot_effects(type = 'bar')
+
+model_stats |> filter(grepl('beta', var)) |> 
+  mutate(varname = gsub('log.', '', varname)) |> 
+  plot_effects(type = 'distribution', mod = mod_res_final)
+ggsave(filename = 'fig/effect_size.png', width = 5.5, height = 4, units = 'in')
+
+# rip rap plot-----------
+riprap = tibble(WY = c(2002, 2012, 2018),
+                length.m = c(81134.76, 83429.76, 86761.23)) |> 
+  # incorporate older estimates from figure: (1935 = 0, but unsure when first riprap started)
+  bind_rows(
+    tibble(WY = c(1962, 1978, 1987),
+           length.m = c(18, 29, 40) * 1609.344)) |> 
+  arrange(WY)
+ggplot(riprap, aes(WY, length.m/1000)) + geom_smooth(method = 'glm') + geom_point() 
+
